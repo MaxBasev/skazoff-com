46d1e4520166bacfbc7d055da3f641e267e43878
Max Basev <maxbasev@gmail.com>
2023-01-19 22:39:09 +0600

    add slider
    from swiperjs-com

--- blog/components/top-section.js
+++ blog/components/top-section.js
@@ -1,13 +1,40 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Author from './_child/author'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
+// Import Swiper styles
+import 'swiper/css';
 
 export default function topSection() {
+    
+    SwiperCore.use([Autoplay]);
+
+    const bg = {
+        background: "url('/images/moon-marek-okon.jpg') no-repeat right center",
+    }
     return(
-        <section className="py-16">
+        <section className="py-16" style={bg}>
             <div className="container mx-auto md:px-20">
                 <h1 className="font-bold text-4xl pb-12 text-center">Last post Trololo</h1>
 
-            {Slide()}
+                <Swiper
+                slidesPerView={1}
+                loop={true}
+                autoplay={{ 
+                    delay: 3000 
+                }}
+                >
+                <SwiperSlide>{Slide()}</SwiperSlide>
+                <SwiperSlide>{Slide()}</SwiperSlide>
+                <SwiperSlide>{Slide()}</SwiperSlide>
+                <SwiperSlide>{Slide()}</SwiperSlide>
+                </Swiper>
+
+
+            
 
 
             </div>
@@ -32,7 +59,7 @@ function Slide() {
                 <p className='text-gray-500 py-3'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </p>
-                <h2>author</h2>
+                <Author></Author>
             </div>
         </div>
     )
